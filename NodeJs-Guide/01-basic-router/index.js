e638ec50bec0df6642d44d1a458d496d349a8d05
BigTear <54252779+BigTear@users.noreply.github.com>
2021-05-01 18:08:49 +0800

    NodeGuide-page35

--- NodeJs-Guide/01-basic-router/index.js
+++ NodeJs-Guide/01-basic-router/index.js
@@ -1,7 +1,19 @@
 var http = require('http');
-http.createServer(function(request, response) {
-	response.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+	console.log('request event');
+	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello World\n');
-}).listen(8124);
+	
+});
+
+server.on('connection', function(){
+	console.log('connection event');
+});
+
+server.listen(8124, function(){
+	console.log('listening event');
+});
+
+console.log('Server running on http://localhost:8124');
