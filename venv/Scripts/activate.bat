206d0a34cbfa2d4db59d21f10a8c06468546779c
BigTear <54252779+BigTear@users.noreply.github.com>
2021-08-03 20:14:25 +0800

    fix bugs

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\tizen\Documents\GitHub\WebRoad\Python\WebRoad\venv
+set VIRTUAL_ENV=D:\tizen\Documents\GitHub\WebRoad\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
