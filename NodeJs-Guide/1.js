1aa7177c208b1fd4ca093474b9407fcb81b4df8d
BigTear <54252779+BigTear@users.noreply.github.com>
2022-02-14 18:07:25 +0800

    清理

--- NodeJs-Guide/1.js
+++
@@ -1,9 +0,0 @@
-function print(a) {
-  console.log(a);
-}
-// 非ASCII码字符转为base64编码
-function toBase64(str) {
-  return btoa(unescape(encodeURIComponent(str)));
-}
-
-print(toBase64('你好'));
--- WebPracticalCases-Senior/4.4-FirstNodeApp/app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-http.createServer(function(request, response) {
-	response.write("Helloq");
-	response.end();
-}).listen(8080);
-
-console.log("http://localhost:8080/");
